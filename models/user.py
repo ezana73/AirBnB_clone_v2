a2fa8c701e4cfb21037c05b8cbb0747132a69057
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2023-06-21 09:37:53 -0700

    Try Fixing Task 8 errors

--- models/user.py
+++ models/user.py
@@ -17,4 +17,4 @@ class User(BaseModel, Base):
     places = relationship("Place", cascade='all, delete',
                           backref="user")
     #reviews = relationship("Review", cascade='all, delete',
-                           backref="user")
+    #                      backref="user")
