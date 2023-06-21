c953785de49822ac58d931bfe0f9c5d53ad60eae
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2023-06-20 17:13:21 -0700

    Delete swp files

--- models/city.py
+++ models/city.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    state_id = Column(String(60), ForeignKey(states.id))
+    name = Column(String(128), nullable=False)
