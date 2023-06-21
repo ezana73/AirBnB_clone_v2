98efe746a9204ca74751a038fa2fab6f6c1cc9c2
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-06-21 00:57:25 +0000

    Update City and State

--- models/city.py
+++ models/city.py
@@ -3,7 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = Column(String(60), ForeignKey(states.id))
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
